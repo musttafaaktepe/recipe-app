c6adac7f2f31e4d4ff7d8f67b135614890e71d7e
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-16 22:37:11 +0300

    v24

--- recipeapp/src/pages/about/About.jsx
+++ recipeapp/src/pages/about/About.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+
 
 const About = () => {
+  let navigate = useNavigate();
   return (
-    <div>About</div>
+    <>
+   
+    <div >
+      <div >
+        <div>
+          
+          <div >
+          <h2>Welcome To Mustafa AKTEPE 
+            
+            Profile</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, itaque modi! Assumenda quasi iusto numquam, facere officiis sapiente asperiores totam magni consectetur eveniet veniam! Accusantium ullam corrupti voluptatem nemo dolore?</p>
+          </div>
+          
+        </div>
+      <button onClick={() => {
+        navigate(-1)
+      }} >Go back</button>
+      </div>
+    </div>
+    </>
+    
   )
 }
 
-export default About;
+export default About
