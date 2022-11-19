172e43ba060a7251fd7b09c48c3627d39f51293c
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-19 11:54:50 +0300

    v25

--- recipeapp/src/pages/about/About.jsx
+++ recipeapp/src/pages/about/About.jsx
@@ -1,33 +1,50 @@
-import React from 'react'
-import { Navigate, useNavigate } from 'react-router-dom'
-
-
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { SiGmail, SiLinkedin, SiGithub } from "react-icons/si";
 
 const About = () => {
   let navigate = useNavigate();
   return (
     <>
-   
-    <div >
-      <div >
+      <div>
         <div>
-          
-          <div >
-          <h2>Welcome To Mustafa AKTEPE 
-            
-            Profile</h2>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, itaque modi! Assumenda quasi iusto numquam, facere officiis sapiente asperiores totam magni consectetur eveniet veniam! Accusantium ullam corrupti voluptatem nemo dolore?</p>
+          <div>
+            <div>
+              <h2>Welcome To Mustafa AKTEPE Profile</h2>
+              <a href="mailto:musttafaaktepe@gmail.com">
+                <SiGmail
+                  className="logo-gmail logo"
+                  style={{ color: "#c5221f", cursor: "pointer" }}
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/musttafaaktepe/"
+                target="blank"
+              >
+                <SiLinkedin
+                  className="logo-linkedin logo"
+                  style={{ color: "#2868b2", cursor: "pointer" }}
+                />
+              </a>
+              <a href="https://github.com/musttafaaktepe" target="blank">
+                <SiGithub
+                  className="logo-gitgub logo"
+                  style={{ color: "#000", cursor: "pointer" }}
+                />
+              </a>
+            </div>
           </div>
-          
+          <button
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Go back
+          </button>
         </div>
-      <button onClick={() => {
-        navigate(-1)
-      }} >Go back</button>
       </div>
-    </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default About
+export default About;
